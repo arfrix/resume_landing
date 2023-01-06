b5304d892cd77cc7cf004ccf6cda1b0927a065d7
pouya <hassanalizadeh@gmail.com>
2023-01-06 13:17:13 +0330

    combined sections

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log("test");
+const buttons = document.querySelectorAll(".wish-list-btn");
+var screen = window.innerWidth;
+
+
+function removeItemsHandler(element) {
+    const firstSiblingNode = element.parentNode.nextElementSibling;
+    const thirdSiblingNode = element.parentNode.previousElementSibling;
+    firstSiblingNode.classList.add("hideElement");
+    thirdSiblingNode.classList.add("hideElement");
+    element.classList.remove("active-btn", "no-hover");
+}
+function ShowItemsHandler(e) {
+    for (let element of buttons) {
+        removeItemsHandler(element)
+    }
+    const firstSiblingNode = e.target.parentNode.nextElementSibling;
+    const thirdSiblingNode = e.target.parentNode.previousElementSibling;
+    firstSiblingNode.classList.remove("hideElement");
+    thirdSiblingNode.classList.remove("hideElement");
+    //add animations
+    if (screen > 700) {
+        thirdSiblingNode.classList.add("fadeInLeft");
+        firstSiblingNode.classList.add("fadeInRight");
+    } else {
+        thirdSiblingNode.classList.add("fadeInDown");
+        firstSiblingNode.classList.add("fadeInDown");
+    }
+    e.target.classList.add("active-btn", "no-hover");
+}
+
+
+
+for (let button of buttons) {
+    button.addEventListener("click", ShowItemsHandler)
+    //add no-hover style for first wish-list-btn (before any click)
+    if (button.classList.contains("active-btn")) {
+        button.classList.add("no-hover")
+    }
+}
